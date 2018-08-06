5e30065372d83e8cf624959c4686c254dc559482
aanupsharan <anup.ambula@gmail.com>
2018-08-05 21:10:03 -0500

    added HttpServlet functionality and bringing the data from the model

--- WebContent/WEB-INF/view/helloworld.jsp
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -3,5 +3,6 @@
 	<body>
 		<p>Hello World of Spring</p>
 		<p>Student Name : ${param.studentName}</p>
+		<p>The Message is : ${message}</p>
 	</body>
 </html>
--- src/com/anup/springdemo/mvc/HelloWorldController.java
+++ src/com/anup/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.anup.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +16,16 @@ public class HelloWorldController {
 	
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShout(HttpServletRequest request, Model model) {
+		String theName = request.getParameter("studentName");
+		theName = theName.toUpperCase();
+		String result = "Yo! " + theName;
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 }
