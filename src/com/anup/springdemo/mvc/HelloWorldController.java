909c0f66940b251272ea3272a1f6a535d2561961
aanupsharan <anup.ambula@gmail.com>
2018-08-11 18:12:46 -0500

    added the parameters for the parent mapping in the controller

--- src/com/anup/springdemo/mvc/HelloWorldController.java
+++ src/com/anup/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("hello")
 public class HelloWorldController {
 
 	@RequestMapping("/showForm")
@@ -24,6 +26,17 @@ public class HelloWorldController {
 		String theName = request.getParameter("studentName");
 		theName = theName.toUpperCase();
 		String result = "Yo! " + theName;
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		theName = theName.toUpperCase();
+		String result = "Hey My Friend from v3! " + theName;
 		model.addAttribute("message", result);
 		
 		return "helloworld";
