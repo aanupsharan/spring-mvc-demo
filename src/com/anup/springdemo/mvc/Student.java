2217c0ae895b239def8dbf45d1a41c2ae9bf415d
aanupsharan <anup.ambula@gmail.com>
2018-08-11 20:54:44 -0500

    Bringing the data from the LinkedHashMap

--- src/com/anup/springdemo/mvc/Student.java
+++ src/com/anup/springdemo/mvc/Student.java
@@ -1,12 +1,21 @@
 package com.anup.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	private String firstName;
 	private String lastName;
 	private String country;
 	
+	private LinkedHashMap<String, String> countryOptions;
+	
 	Student() {
-		
+		countryOptions = new LinkedHashMap<String,String>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States");
 	}
 
 	public String getFirstName() {
@@ -32,5 +41,8 @@ public class Student {
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
 }
