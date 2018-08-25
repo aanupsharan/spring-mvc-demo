69980ff4d083a3e9cc4f08d8011285a5db48ef28
aanupsharan <anup.ambula@gmail.com>
2018-08-25 12:47:56 -0500

    added the @InitBinder for validating the whitespaces

--- src/com/anup/springdemo/mvc/CustomerController.java
+++ src/com/anup/springdemo/mvc/CustomerController.java
@@ -1,15 +1,25 @@
 package com.anup.springdemo.mvc;
 
 import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@InitBinder
+	public void InitBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@RequestMapping("showForm")
 	public String showForm(Model theModel) {
